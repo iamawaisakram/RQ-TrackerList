c6af449fa601177c526ac2e917e6e804efb76ccb
Awais Mughal <iamawaisakram@gmail.com>
2022-11-23 18:28:42 +0500

    👉🏻Implements Query Client 👉🏻 Fetches data and displays it

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,34 @@
-import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import { IssueItem } from "./IssueItem";
 
 export default function IssuesList() {
-  return (
-    <div>
-      <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
-    </div>
-  );
+    const issuesQuery = useQuery(["issues"], () =>
+        fetch("/api/issues").then((res) => res.json())
+    );
+
+    console.log(issuesQuery.data);
+
+    return (
+        <div>
+            <h1>Issues List</h1>
+            {issuesQuery.isLoading ? (
+                <p>Loading ...</p>
+            ) : (
+                issuesQuery.data.map((issue) => (
+                    <ul className="issues-list" key={issue.id}>
+                        <IssueItem
+                            title={issue.title}
+                            number={issue.number}
+                            assignee={issue.assignee}
+                            commentCount={issue.comments.length}
+                            createdDate={issue.createdDate}
+                            createdBy={issue.createdBy}
+                            labels={issue.labels}
+                            status={issue.status}
+                        />
+                    </ul>
+                ))
+            )}
+        </div>
+    );
 }
